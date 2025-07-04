ccbcd36b7c91e2c834cae5189ff0e4e033003666
Imran Zia <imranzia103@gmail.com>
2025-07-04 22:59:30 +0500

    Add files via upload
    update product features added

---
+++ main.jsx
@@ -0,0 +1,75 @@
+import React from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+import store from "./components/redux/store.js";
+import { Provider } from 'react-redux';
+import { Route, RouterProvider, createRoutesFromElements } from "react-router";
+import { createBrowserRouter } from "react-router-dom";
+
+
+//Auth
+
+import AdminRoute from './components/pages/Admin/AdminRoutes.jsx';
+import CategoryList from './components/pages/Admin/CategoryList.jsx';
+import ProductList from './components/pages/Admin/ProductList.jsx';
+import ProductUpdate from './components/pages/Admin/ProductUpdate.jsx'
+import AllProducts from './components/pages/Admin/AllProducts.jsx';
+
+
+//Resticted
+
+import Login from './components/pages/Auth/Login.jsx';
+import Register from './components/pages/Auth/Register.jsx';
+import PrivateRoute from './components/pages/Auth/PrivateRoute.jsx';
+import Profile from "./components/pages/User/Profile.jsx"
+
+
+
+import Home from './components/pages/Home.jsx';
+
+
+const router = createBrowserRouter(
+  createRoutesFromElements( 
+    <Route path="/" element={<App />}>
+          <Route index={true} path="/" element={<Home />} />
+          <Route path='/login' element={<Login/>} />
+           <Route path='/register' element={<Register/>} />
+
+
+
+           <Route path='' element={<PrivateRoute/>}>
+           <Route path='/profile' element={<Profile/>}/>
+
+            <Route path='' element={<AdminRoute/>}/>
+            
+            <Route path="/categorylist" element={<CategoryList />} />
+            <Route path="/productlist" element={<ProductList />} />
+            <Route path="/AllProducts" element={<AllProducts />} />
+            <Route path="/ProductUpdate/:_id" element={<ProductUpdate />} />
+
+           
+
+
+
+
+
+
+
+           </Route>
+
+
+
+  </Route>)
+
+);
+
+createRoot(document.getElementById('root')).render(
+  
+  <Provider store={store}>
+    
+      <RouterProvider router={router} />
+    
+  </Provider>
+  
+)
---
+++ redux/constants.js
@@ -0,0 +1,9 @@
+export const BASE_URL = "";
+export const USERS_URL = "/api/v1/users";
+export const CATEGORY_URL = "/api/v1/category";
+
+
+export const PRODUCT_URL = "/api/v1/products";
+export const UPLOAD_URL = "/api/v1/upload";
+export const ORDERS_URL = "/api/orders";
+export const PAYPAL_URL = "/api/config/paypal";
